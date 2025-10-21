b39447061d7e54b36832509c4e0992ef8e816f18
AndyLipo <andliporace@hotmail.com>
2025-10-20 21:45:57 -0300

    Listo para deploy

--- src/Components/ContactHome/FormCheckbox.jsx
+++ src/Components/ContactHome/FormCheckbox.jsx
@@ -1,28 +1,56 @@
-import { useRef } from "react";
-import ReCAPTCHA from "react-google-recaptcha"
+// import { useRef } from "react";
+// import ReCAPTCHA from "react-google-recaptcha"
 
-// const API_KEY = "6LeJQ64rAAAAAOG1wOQPa9fBgfnb0e0cxOJ4qB_M"
+// // const API_KEY = "6LeJQ64rAAAAAOG1wOQPa9fBgfnb0e0cxOJ4qB_M"
+
+// const FormCheckbox = () => {
+//     const captcha = useRef(null)
+//     const onChange = () => {
+//         if (captcha.current.getValue()) {
+//             // console.log("El usuario no es un robot");
+//             console.clear
+//         }
+
+//     }
+//     return (
+//         <div className={`flex items-start space-x-3`}>
+//             <ReCAPTCHA
+//                 ref={captcha}
+//                 className="mt-1 bg-white"
+//                 sitekey='6Ld2LbArAAAAAAHrZ6U-932opv0vWrQ_Q-vkFnrT'
+//                 onChange={onChange}
+
+//             />
+//         </div>
+//     )
+// }
+
+// export default FormCheckbox
+
+import { useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const FormCheckbox = () => {
-    const captcha = useRef(null)
+    const captcha = useRef(null);
+
     const onChange = () => {
         if (captcha.current.getValue()) {
-            // console.log("El usuario no es un robot");
+            console.log("Captcha verificado");
+            // Aquí puedes habilitar el envío del formulario
             console.clear
-        }
 
-    }
+        }
+    };
+
     return (
         <div className={`flex items-start space-x-3`}>
             <ReCAPTCHA
                 ref={captcha}
-                className="mt-1 bg-white"
                 sitekey='6Ld2LbArAAAAAAHrZ6U-932opv0vWrQ_Q-vkFnrT'
                 onChange={onChange}
-
             />
         </div>
-    )
-}
+    );
+};
 
-export default FormCheckbox
+export default FormCheckbox;
