4cdef6d6ca687a9df543f148fe44aa35a5391aac
AndyLipo <andliporace@hotmail.com>
2026-04-02 20:08:04 -0300

    Fixed WorkPng

--- src/Components/ContactHome/FormTextarea.jsx
+++ src/Components/ContactHome/FormTextarea.jsx
@@ -1,5 +1,5 @@
-import { Label } from "@/components/ui/label"
-import { Textarea } from "@/components/ui/textarea"
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
 
 const FormTextarea = ({
     id,
