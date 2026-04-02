d280d19ca7a697835a70590a4fe1d6167705f213
AndyLipo <andliporace@hotmail.com>
2026-04-02 19:49:25 -0300

    Fixed FooterNavigation

--- src/Components/Footer/FooterNavigation.jsx
+++ src/Components/Footer/FooterNavigation.jsx
@@ -1,11 +1,6 @@
 import React from "react"
-import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-} from "@/components/ui/navigation-menu"
 import { Link } from "react-router-dom"
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "../ui/navigation-menu"
 
 const FooterNavigation = ({
     links = [],
